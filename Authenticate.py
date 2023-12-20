1950c9721429f97fefa165397d63a5c2f56b5e1c
charankumar <Administrator@MCA.COM>
2023-12-20 16:28:42 +0530

    first commit

---
+++ Authenticate.py
@@ -0,0 +1,11 @@
+user={'username':'venket','passward':'Red123'}
+user=input('Enter user name:')
+passward=input('enter passward:')
+if username!=user['username']:
+    print('username inncrect')
+else:
+    print('username is vallid')
+if passward!=user['passward']:
+    print('passward inncrect')
+else:
+    print('passward is not currect')
