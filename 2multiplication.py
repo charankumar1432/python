d6af676f34912328e877a56f8eb56a218b6d47dd
charankumar1432 <154216159+charankumar1432@users.noreply.github.com>
2024-01-09 07:34:28 +0530

    Update2multiplication.py

--- 2multiplication.py
+++ 2multiplication.py
@@ -2,7 +2,7 @@ s=('he@idg#d14')
 i=0
 out=''
 while i<len(s):
-       if not ('A'<=s<='Z' or 'a'<=s<'a' or '0'<=s<='9'):
+       if not ('A'<=s<='Z' or 'a'<=s<='z' or '0'<=s<='9'):
               out=out+''
        else:
             out=out+s[i]
